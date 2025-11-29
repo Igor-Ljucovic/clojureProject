aeda4657a0912688558c25c6f6ac53a99b482bb9
Igor <igorljucovic99@gmail>
2025-11-29 10:47:47 +0100

    add project template

---
+++ clojure_university_project/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure_university_project
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ clojure_university_project/test/clojure_university_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-university-project.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-university-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
